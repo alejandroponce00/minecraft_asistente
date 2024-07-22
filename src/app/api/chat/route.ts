38a7e6cd7fd11db958ce380ea67ae556f6d32bf3
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-07-22 16:50:47 -0300

    first commit

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,40 @@
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import OpenAI from 'openai';
+
+const perplexity = new OpenAI({
+  apiKey: process.env.PERPLEXITY_API_KEY || '',
+  baseURL: 'https://api.perplexity.ai',
+});
+
+export async function POST(req: Request) {
+  try {
+    // Extract the `messages` from the body of the request
+    const { messages } = await req.json();
+
+    // Add content with a role and instructions
+    const instructions = {
+      role: 'system',
+      content: 'eres un asistente muy malhumorado que no quiere ayudar a nadie y contesta de muy mala manera',
+    };
+
+    // Append instructions to the beginning of the messages array
+    const enhancedMessages = [instructions, ...messages];
+
+    // Request the OpenAI-compatible API for the response based on the prompt
+    const response = await perplexity.chat.completions.create({
+      model: 'llama-3-sonar-small-32k-chat',
+      stream: true,
+      messages: enhancedMessages,
+      max_tokens: 300,
+    });
+
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response);
+
+    // Create a new Response object with the stream
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    // Handle errors by returning a response with a suitable status code and message
+    return new Response(JSON.stringify({ error: 'Internal Server Error' }), { status: 500 });
+  }
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useChat } from 'ai/react';
+import Image from "next/image";
+import Fondo from './components/fondo/fondo';
+
+
+
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit,isLoading } = useChat();
+
+  return (
+    <div className='text-center rounded-lg p-4 mx-2.5 '>
+     <Fondo />
+      <h1 className='text-5xl text-center my-3 text-slate-800 font-bold mb-10'>Ariel GPT</h1>
+      <h6 className=' text-2xl text-center my-3 text-slate-600 mb-10'>Tu asistente con mala onda</h6>
+    
+      <div className='mb-4 max-h-96 h-full overflow-y-scroll '>
+      
+        {messages.map((m) => (
+          <div 
+            key={m.id} 
+            className={`p-2 my-2 rounded-md ${m.role === 'user' ? 'bg-blue-200 text-left opacity-60' : 'bg-green-200 text-right opacity-60'}`}
+          >
+            {m.role === 'user' ? 'YO: ' : 'ASISTENTE: '}
+            {m.content}
+          </div>
+        ))}
+        
+      </div>
+      <form onSubmit={handleSubmit} className='flex'>
+        <input
+          value={input}
+          placeholder="Pregúntame algo"
+          onChange={handleInputChange}
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+        <button className='bg-amber-400 text-white px-3 py-2 rounded-md'
+        
+        >enviar</button>
+        
+      </form>
+    </div>
+  );
+}
