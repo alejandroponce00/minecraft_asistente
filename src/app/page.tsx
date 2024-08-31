82b470e2d750530092a1eb1f7b628b6a7509f58b
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-08-31 11:33:16 -0300

    arreglo de scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,36 @@
 'use client';
 
 import { useChat } from 'ai/react';
-import Image from "next/image";
+import { useEffect, useRef } from 'react';
 import Fondo from './components/fondo/fondo';
 import { FlipWords } from "../app/components/ui/flip-words";
 
-
-
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit,isLoading } = useChat();
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
   const words = ["Asistente", "Experto", "Sabelotodo"];
+  
+  const messageContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Desplazarse al final cuando cambian los mensajes
+    messageContainerRef.current?.scrollTo(0, messageContainerRef.current.scrollHeight);
+  }, [messages]);
 
   return (
     <div className='text-center rounded-lg p-4 mx-2.5 '>
-     <Fondo />
-      <h1 className='text-5xl text-center my-3 text-green-500 font-bold mb-10'><FlipWords words={words} /> <br /> de Minecraft</h1>
+      <Fondo />
+      <h1 className='text-5xl text-center my-3 text-green-500 font-bold mb-10'>
+        <FlipWords words={words} /> <br /> de Minecraft
+      </h1>
       
-      <h6 className=' text-2xl text-center my-3 text-slate-600 mb-10'>Hola Ciro,preguntame algo sobre minecraft</h6>
+      <h6 className='text-2xl text-center my-3 text-slate-600 mb-10'>
+        Hola Ciro, pregúntame algo sobre Minecraft
+      </h6>
     
-      <div className='mb-4 max-h-96 h-full overflow-y-scroll '>
-      
+      <div 
+        ref={messageContainerRef} 
+        className='mb-4 max-h-96 h-full overflow-y-scroll'
+      >
         {messages.map((m) => (
           <div 
             key={m.id} 
@@ -29,8 +40,8 @@ export default function Chat() {
             {m.content}
           </div>
         ))}
-        
       </div>
+
       <form onSubmit={handleSubmit} className='flex'>
         <input
           value={input}
@@ -38,10 +49,9 @@ export default function Chat() {
           onChange={handleInputChange}
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         />
-        <button className='bg-amber-400 text-white px-3 py-2 rounded-md'
-        
-        >enviar</button>
-        
+        <button className='bg-amber-400 text-white px-3 py-2 rounded-md'>
+          enviar
+        </button>
       </form>
     </div>
   );
